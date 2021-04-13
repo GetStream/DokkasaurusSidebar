ea4fd6ce505da278de7faabafccd049e4208ee59
Leandro Borges Ferreira <lehen01@gmail.com>
2021-04-13 09:12:27 -0300

    project rename

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="dokkasauros",
-    version="0.1.3",
-    py_modules=["dokkasauros"],
+    name="dokkasaurus",
+    version="0.2.0",
+    py_modules=["dokkasaurus"],
     include_package_data=True,
     install_requires=["click"],
     entry_points="""
         [console_scripts]
-        dokkasauros=dokkasauros:cli
+        dokkasaurus=dokkasaurus:cli
     """,
 )
